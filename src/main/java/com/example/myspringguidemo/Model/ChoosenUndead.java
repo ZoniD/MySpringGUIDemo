6b6783b672eb0cc40ff2a59773a0753a6c8e02d5
ZoniD <deba0001@stud.kea.dk>
2025-10-17 12:08:29 +0200

    new update

---
+++ src/main/java/com/example/myspringguidemo/Model/ChoosenUndead.java
@@ -0,0 +1,24 @@
+package com.example.myspringguidemo.Model;
+
+public class ChoosenUndead {
+
+        private boolean defeatedGargoyel;
+        private boolean defeatedChaos;
+
+        // Getters and setters
+        public boolean isDefeatedGargoyel() {
+            return defeatedGargoyel;
+        }
+
+        public void setDefeatedGargoyel(boolean defeatedGargoyel) {
+            this.defeatedGargoyel = defeatedGargoyel;
+        }
+
+        public boolean isDefeatedChaos() {
+            return defeatedChaos;
+        }
+
+    public void setDefeatedChaos(boolean defeatedChaos) {
+        this.defeatedChaos = defeatedChaos;
+    }
+}
